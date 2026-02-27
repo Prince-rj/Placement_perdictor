e86f948f0c53a4def160482b604f99f9648a3698
Prince-rj <88528048+Prince-rj@users.noreply.github.com>
2026-02-27 23:14:24 +0530

    test code

--- CPP_Based_Algo/Perceptron.cpp
+++ CPP_Based_Algo/Perceptron.cpp
@@ -2,7 +2,16 @@
 
 using namespace std;
 
+
+int forward_pass_Y( double iq, double cgpa){
+    cout<<"sanu";
+    return 0;
+}
+
 int main(){
     cout<<"perceptron";
+
+    forward_pass_Y(0,0);
+    cout<<forward_pass_Y(0,0);
     return 0;
 }
