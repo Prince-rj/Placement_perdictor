2955b49f40bf6a55e07725bdee425a0bf2a7e7a7
Sanu Kumar <sanushusma2190@gmail.com>
2026-02-28 11:45:59 +0530

    test

--- CPP_Based_Algo/Perceptron.cpp
+++ CPP_Based_Algo/Perceptron.cpp
@@ -2,16 +2,26 @@
 
 using namespace std;
 
+//variables:
+double X1, X2;
+double W1, W2, B;
+//functions:
+//Y function:
 
-int forward_pass_Y( double iq, double cgpa){
-    cout<<"sanu";
-    return 0;
+double Forward_pass( double X1, double X2){
+    return X1*W1+X2*W2+B;
 }
 
 int main(){
-    cout<<"perceptron";
 
-    forward_pass_Y(0,0);
-    cout<<forward_pass_Y(0,0);
+    //User input code:
+    cout<<"perceptron"<<endl;
+    cout<<"Type your IQ here: "<<endl;
+    cout<<"Type your CGPA here: "<<endl;
+
+    //Forward pass calculation:
+    double c= Forward_pass(X1,X2);
+    cout<<c<<endl;
+
     return 0;
 }
